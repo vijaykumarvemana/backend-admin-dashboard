c7f0b05e15ff81f9648ad318b601ba453f9d06af
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2021-12-15 16:22:49 +0100

    updated

--- src/transactions/index.js
+++ src/transactions/index.js
@@ -26,6 +26,44 @@ transactionRouter.post('/', async (req, res, next) => {
     }
 });
 
+transactionRouter.get('/:id', async (req, res, next) => {
+    try {
+        const transaction = await TransactionModel.findById(req.params.id).populate('customer product');
+        if (!transaction) {
+            throw createHttpError(404, 'Transaction not found');
+        }
+        res.send(transaction);
+    } catch (err) {
+        next(err);
+    }
+});
+
+transactionRouter.put('/:id', async (req, res, next) => {
+    try {
+    
+        const transaction = await TransactionModel.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        if (!transaction) {
+            throw createHttpError(404, 'Transaction not found');
+        }
+        res.send(transaction);
+    } catch (err) {
+        next(err);
+    }
+});
+
+transactionRouter.delete('/:id', async (req, res, next) => {
+    try {
+        const transaction = await TransactionModel.findByIdAndDelete(req.params.id);
+        if (!transaction) {
+            throw createHttpError(404, 'Transaction not found');
+        }
+        res.send(transaction);
+    } catch (err) {
+        next(err);
+    }
+});
+
+
 
 
       
